7d4a9a8bb8d8a557e29516215f8d7597aaf0c73b
Nick Piggott <nick@piggott.eu>
2016-09-05 08:43:09 +0100

    Update to ODR-DabMux v1.1.0 / Debounce
    Now uses the start_in and stop_in function of ODR-DabMux announcement
    control to create a delay to realign with the audio.
    
    Adds a debounce. An inactive request is held back 2 seconds, and if
    another active request is made in that time window, the inactive is
    ignored.

--- announcements.php
+++ announcements.php
@@ -1,78 +1,116 @@
 <?php
 
-/* There should be a README with this file, to explain how to configure dab-odrmux to signal announcements and how to call this script */
-
 /* Change the variable $announcement_command_suffix to reflect the value used in your odr-dabmux configuation */
 $announcement_command_suffix = "_traffic_announcement";
+$announcement_directory = "announcements/";
 
 $active = ($_GET["active"]);
 $station = ($_GET["station"]);
-$http_output = "";
+$delay = ($_GET["delay"]);
+$http_output = "Unknown fatal error. Check error logs";
+$log_output = "";
 $http_code = 500;
+$action = "active 0";
+
+/* Allow the script to run beyond the socket close */
+ignore_user_abort(true);
 
 if ($active == false) $active = "0";
+if ($delay == false) $delay = 0;
 if ($station==false) {
-	$http_output .= "No station defined in URL.";
+	$http_output = "No station defined";
 	$http_code = 400;
 } else {
-	$service_port = "12721";
-	$address = "127.0.0.1";
-
-	$socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
-	$http_output .= "Attempting to create socket : ";
-	if ($socket === false) {
-	    $http_output .= "socket_create() failed: reason: " . socket_strerror(socket_last_error()) . "<br />\n";
-	} else {
-	    $http_output .= "OK.<br />\n";
-	}
-
-	$http_output .= "Attempting to connect to '$address' on port '$service_port' : ";
-	$result = socket_connect($socket, $address, $service_port);
-	if ($result === false) {
-	    $http_output .= "socket_connect() failed.<br />Reason: ($result) " . socket_strerror(socket_last_error($socket)) . "<br />\n";
-	} else {
-	    $http_output .= "OK.<br />\n";
-	}
-
-	if (read_response($socket)) break;
-
-	send_command($socket,"set ".$station.$announcement_command_suffix." active ".$active);
-	if (read_response($socket)) break;
-
-	send_command($socket,"quit");
-	if (read_response($socket)) break;
-
-	$http_output .= "Closing socket...<br />\n";
-	socket_close($socket);
-	$http_code =200;
+	$http_output = "Attempting to set ".$station.$announcement_command_suffix." active " .$active." in ".$delay." seconds";
+	$http_code = 200;
 }
+
 http_response_code($http_code);
-
-function read_response($socket) {
-	global $http_output;
-    $out = "";
-	$start = time();
-	$timed_out = false;
-    while (substr($out,-2) !== "> ") {
-		$timed_out = ((time()-$start)>5);
-		if (false == (socket_recv($socket, $out, 2048, false))) break;
-		$http_output .= str_replace("\n","<br />\n",$out);
-	}
-return $timed_out;
-}
-
-function send_command($socket,$in) {
-	global $http_output;
-    $http_output .= "<b>" . $in . "</b><br />\n";
-    socket_write($socket, $in."\n", strlen($in)+1);
-return;
-}
-
 ?>
 <html>
 <head>
 <title>Announcement Flag Controller</title>
 <body>
 <h2>Announcement Flag Controller</h2>
-<?php echo $http_output ?>
+<?php echo $http_output . "<\br> \n";
+
+
+if ($http_code ==200) {
+
+	if ($active == false) {
+		$lock_handle = fopen($announcement_directory.$station."_ta_pending_inactive","c");
+		fclose($lock_handle);
+		sleep(2);
+		if (!file_exists($announcement_directory.$station."_ta_pending_inactive")) {
+		    	$log_output = ("Announcements :" .$station." announcement set active 0 aborted as active 1 requested");
+		    	syslog(LOG_ERR,$log_output);
+		    	exit($log_output);
+		} 
+	}
+
+	unlink($announcement_directory.$station."_ta_pending_inactive"); 
+
+	$service_port = "12721";
+	$address = "127.0.0.1";
+
+	$socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+	if ($socket == false) {
+	    $log_output = "Announcements :" .$station." socket_create() failed: Reason: " . socket_strerror(socket_last_error());
+	    syslog(LOG_ERR,$log_output);
+	    exit($log_output);
+	} 
+
+	$result = socket_connect($socket, $address, $service_port);
+	if ($result == false) {
+	    $log_output = "Announcements: " .$station. " socket_connect() failed. Reason: " . socket_strerror(socket_last_error($socket));
+	    syslog(LOG_ERR,$log_output);
+	    exit($log_output);
+	}
+
+	if (read_response($socket)) break;
+
+	if ($delay > 0) {
+		if ($active == 1) {
+			$action = "start_in ";
+		} else {
+			$action = "stop_in ";
+		}
+		$action .= $delay;
+	} else {
+		$action = "active" . $active;
+	}
+
+	send_command($socket,"set ".$station.$announcement_command_suffix." ".$action);
+	if (read_response($socket)) break;
+
+	send_command($socket,"quit");
+	if (read_response($socket)) break;
+
+	$log_output = "Announcements: " . $station . " sent announcement command " .$action;
+        syslog(LOG_INFO,$log_output);
+	socket_close($socket);
+}
+
+
+
+echo $log_output . "<\br> \n";
+
+?>
 </body></html>
+<?php
+
+function read_response($socket) {
+    	$out = "";
+	$start = time();
+	$timed_out = false;
+    	while (substr($out,-2) !== "> ") {
+		$timed_out = ((time()-$start)>5);
+		if (false == (socket_recv($socket, $out, 2048, false))) break;
+	}
+return $timed_out;
+}
+
+function send_command($socket,$in) {
+     	socket_write($socket, $in."\n", strlen($in)+1);
+return;
+}
